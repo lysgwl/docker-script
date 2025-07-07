03cf2df605a27857b3471e0ec808c6240c65d295
unknown <lysgwl@163.com>
2025-07-07 10:23:17 +0800

    update script menu

--- freeswitch/usr/libks-v2.0.6/win/build.cmd
+++
@@ -1,8 +0,0 @@
-@REM check and set Visual Studio environment
-CALL msbuild.cmd
-echo %msbuild%
-cmd /C %msbuild% libks.sln /p:Configuration=Debug /p:Platform=Win32 /t:Build
-cmd /C %msbuild% libks.sln /p:Configuration=Debug /p:Platform=x64 /t:Build 
-cmd /C %msbuild% libks.sln /p:Configuration=Release /p:Platform=Win32 /t:Build 
-cmd /C %msbuild% libks.sln /p:Configuration=Release /p:Platform=x64 /t:Build
-echo Done! Packages (zip files) were placed to the "out" folder.
